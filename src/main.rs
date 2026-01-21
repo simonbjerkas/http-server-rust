8f13186aa2a278900b6a4c658c0762e0834e03fc
simonbjerkas <simon.bjerkas10@gmail.com>
2026-01-21 14:46:38 +0100

    returning 200 response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n");
+    stream.write_all(response.as_bytes()).unwrap();
 }
