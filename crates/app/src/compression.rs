5e1f9e375aa226301b05e482c1172bd5405120a4
simonbjerkas <simon.bjerkas10@gmail.com>
2026-02-05 12:08:13 +0100

    handling compression headers in middleware

---
+++ crates/app/src/compression.rs
@@ -0,0 +1,21 @@
+use http_server::{
+    App, Request, Response,
+    middleware::{Next, middleware},
+};
+
+#[middleware]
+fn compression(req: Request, app: &App, next: Next) -> Response {
+    let Some(encoding) = req.headers.get("accept-encoding") else {
+        return next.run(req, app);
+    };
+
+    if encoding != "gzip" {
+        return next.run(req, app);
+    }
+
+    let mut res = next.run(req, app);
+
+    res.headers.insert("content-encoding", "gzip");
+
+    res
+}
