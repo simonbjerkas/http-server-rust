8b779bc1f7e998fa66b7a1ff17bf6d85e1900cc8
simonbjerkas <simon.bjerkas10@gmail.com>
2026-02-05 13:20:55 +0100

    adding connection: close header to res

--- crates/http-server/src/server.rs
+++ crates/http-server/src/server.rs
@@ -4,7 +4,7 @@ pub mod scope;
 
 use std::{
     io::{BufReader, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     sync::Arc,
 };
 
@@ -29,33 +29,41 @@ impl Server {
         let app = Arc::new(self.app);
 
         for stream in listener.incoming() {
-            let mut stream = stream?;
+            let stream = stream?;
             let app = Arc::clone(&app);
 
-            pool.execute(move || {
-                loop {
-                    let reader = BufReader::new(&mut stream);
-                    let request = match Request::build(reader) {
-                        Ok(req) => req,
-                        Err(_) => {
-                            let res = Response::bad();
-                            stream.write_all(&res.to_bytes()).unwrap();
-                            return;
-                        }
-                    };
-
-                    let close = request.headers.close_connection();
-                    let mut res = app.handle(request);
-                    res.finalize();
-
-                    stream.write_all(&res.to_bytes()).unwrap();
-
-                    if close {
-                        break;
-                    }
-                }
-            });
+            pool.execute(move || handle_connection(stream, &app));
         }
         Ok(())
     }
+}
+
+fn handle_connection(mut stream: TcpStream, app: &App) {
+    if process_request(&mut stream, app).is_err() {
+        let res = Response::bad();
+        let _ = stream.write_all(&res.to_bytes());
+    }
+}
+
+fn process_request(stream: &mut TcpStream, app: &App) -> Result<()> {
+    loop {
+        let reader = BufReader::new(&*stream);
+        let req = Request::build(reader)?;
+
+        let close = req.headers.close_connection();
+        let mut res = app.handle(req);
+
+        if close {
+            res.headers.insert("connection", "close");
+        }
+
+        res.finalize();
+        stream.write_all(&res.to_bytes())?;
+
+        if close {
+            break;
+        }
+    }
+
+    Ok(())
 }
